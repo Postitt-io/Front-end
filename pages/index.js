e633d3a44a3c847c4983c70c5b782412a112b4b8
Nathan M <nathanmayall@icloud.com>
2020-11-29 20:29:07 +0000

    working on images

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
+import Image from 'next/image';
+
 import Nav from '../components/nav';
 
 export default function IndexPage() {
   return (
-    <div>
+    <>
       <Nav />
-      <div className="py-20">
+      <div className="py-20 justify-center">
+        <Image
+          src="/default.svg"
+          width={250}
+          height={250}
+          className="bg-white rounded-lg "
+        />
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
-    </div>
+    </>
   );
 }
