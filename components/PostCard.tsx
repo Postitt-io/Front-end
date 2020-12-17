d5c1f440a62635a020a3382539ca792534466e8b
Nathan M <70609396+nathanmayall@users.noreply.github.com>
2020-12-17 23:12:50 +0000

    add vote mechanic and comment count

---
+++ components/PostCard.tsx
@@ -0,0 +1,143 @@
+import dayjs from 'dayjs';
+import Link from 'next/link';
+
+import { Post } from '../types';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import Axios from 'axios';
+import classNames from 'classnames';
+
+dayjs.extend(relativeTime);
+
+interface PostCardProps {
+  post: Post;
+}
+
+const ActionButton = ({ children }) => {
+  return (
+    <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
+      {children}
+    </div>
+  );
+};
+
+export default function PostCard({
+  post: {
+    identifier,
+    slug,
+    title,
+    body,
+    subName,
+    createdAt,
+    voteScore,
+    userVote,
+    commentCount,
+    url,
+    username,
+  },
+}: PostCardProps) {
+  const vote = async (value) => {
+    try {
+      Axios.post('/misc/vote', {
+        identifier,
+        slug,
+        value,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div key={identifier} className="flex mb-4 bg-white rounded">
+      {/* Vote Section */}
+      {/* Plus Button */}
+      <div className="w-10 py-3 text-center bg-gray-200 rounded-l">
+        <div
+          className="w-6 mx-auto text-gray-400 transition duration-75 rounded cursor-pointer hover:bg-gray-300 hover:text-button-blue"
+          onClick={() => vote(1)}
+        >
+          <i
+            className={classNames('far fa-plus-square', {
+              'text-button-blue': userVote === 1,
+            })}
+          ></i>
+        </div>
+        {/* Vote Count */}
+        <p className="text-xs font-bold">{voteScore}</p>
+        {/* Minus Button */}
+        <div
+          className="w-6 mx-auto text-gray-400 transition duration-75 rounded cursor-pointer hover:bg-gray-300 hover:text-red-300"
+          onClick={() => vote(-1)}
+        >
+          <i
+            className={classNames('far fa-minus-square', {
+              'text-red-300': userVote === -1,
+            })}
+          ></i>
+        </div>
+      </div>
+      {/* Post data section */}
+
+      <div className="w-full p-2">
+        <div className="flex items-center">
+          {/* Sub  Icon */}
+          <Link href={`/p/${subName}`}>
+            <img
+              src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+              className="w-6 h-6 mr-1 rounded-full cursor-pointer"
+            />
+          </Link>
+          {/* Sub Name */}
+          <Link href={`/p/${subName}`}>
+            <a className="text-xs font-semibold cursor-pointer hover:underline">
+              p/{subName}
+            </a>
+          </Link>
+          <p className="text-xs text-gray-500">
+            <span className="mx-1">•</span>
+            Posted by
+            {/* Username */}
+            <Link href={`/u/${username}`}>
+              <a className="mx-1 hover:underline">u/{username}</a>
+            </Link>
+            {/* Timestamp */}
+            <Link href={url}>
+              <a className="mx-1 hover:underline">
+                {dayjs(createdAt).fromNow()}
+              </a>
+            </Link>
+          </p>
+        </div>
+        {/* Title */}
+        <Link href={url}>
+          <a className="my-1 text-lg font-semibold">{title}</a>
+        </Link>
+        {/* Body if applicable */}
+        {body && <p className="my-1 text-sm">{body}</p>}
+        <div className="flex">
+          {/* Comments button */}
+          <Link href={url}>
+            <a>
+              <ActionButton>
+                <i className="mr-1 fas fa-comment-alt fa-xs"></i>
+                <span className="font-semibold">
+                  {commentCount} comments
+                </span>
+              </ActionButton>
+            </a>
+          </Link>
+          {/* Share button */}
+          <ActionButton>
+            <i className="mr-1 fas fa-share fa-xs"></i>
+            <span className="font-semibold">Share</span>
+          </ActionButton>
+          {/* Save button */}
+          <ActionButton>
+            <i className="mr-1 fas fa-bookmark fa-xs"></i>
+            <span className="font-semibold">Save</span>
+          </ActionButton>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,27 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import { Fragment, useEffect, useState } from 'react';
 import Axios from 'axios';
 
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-// import { Post } from '../types';
-import { GetServerSideProps } from 'next';
+import { Post } from '../types';
+
+// import { GetServerSideProps } from 'next';
+
+import { useEffect, useState } from 'react';
+import PostCard from '../components/postCard';
 
 dayjs.extend(relativeTime);
 
-export default function IndexPage({ posts }) {
-  // CLIENT SIDE EXAMPLE RENDERING BELOW, PAGE USES SSR
-  // const [posts, setPosts] = useState<Post[]>([]);
+export default function IndexPage() {
+  // CLIENT SIDE RENDERING
+  const [posts, setPosts] = useState<Post[]>([]);
 
-  // useEffect(() => {
-  //   Axios.get('/posts')
-  //     .then((res) => setPosts(res.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    Axios.get('/posts')
+      .then((res) => setPosts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
@@ -30,84 +32,7 @@ export default function IndexPage({ posts }) {
         {/* Posts Feed */}
         <div className="w-160">
           {posts.map((post) => (
-            <div
-              key={post.identifier}
-              className="flex mb-4 bg-white rounded"
-            >
-              {/* Vote Section */}
-              <div className="w-10 text-center bg-gray-200 rounded-l">
-                <p>V</p>
-              </div>
-              {/* Post data section */}
-
-              <div className="w-full p-2">
-                <div className="flex items-center">
-                  {/* Sub  Icon */}
-                  <Link href={`/p/${post.subName}`}>
-                    <img
-                      src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
-                      className="w-6 h-6 mr-1 rounded-full cursor-pointer"
-                    />
-                  </Link>
-                  {/* Sub Name */}
-                  <Link href={`/p/${post.subName}`}>
-                    <a className="text-xs font-semibold cursor-pointer hover:underline">
-                      p/{post.subName}
-                    </a>
-                  </Link>
-                  <p className="text-xs text-gray-500">
-                    <span className="mx-1">•</span>
-                    Posted by
-                    {/* Username */}
-                    <Link href={`/u/${post.username}`}>
-                      <a className="mx-1 hover:underline">
-                        u/{post.username}
-                      </a>
-                    </Link>
-                    {/* Timestamp */}
-                    <Link href={post.url}>
-                      <a className="mx-1 hover:underline">
-                        {dayjs(post.createdAt).fromNow()}
-                      </a>
-                    </Link>
-                  </p>
-                </div>
-                {/* Title */}
-                <Link href={post.url}>
-                  <a className="my-1 text-lg font-semibold">
-                    {post.title}
-                  </a>
-                </Link>
-                {/* Body if applicable */}
-                {post.body && (
-                  <p className="my-1 text-sm">{post.body}</p>
-                )}
-                <div className="flex">
-                  {/* Comments button */}
-                  <Link href={post.url}>
-                    <a>
-                      <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                        <i className="mr-1 fas fa-comment-alt fa-xs"></i>
-                        <span className="font-semibold">
-                          {/* TODO: count comments and display here */}
-                          20 comments
-                        </span>
-                      </div>
-                    </a>
-                  </Link>
-                  {/* Share button */}
-                  <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-share fa-xs"></i>
-                    <span className="font-semibold">Share</span>
-                  </div>
-                  {/* Save button */}
-                  <div className="px-1 py-1 mr-1 text-xs text-gray-400 rounded cursor-pointer hover:bg-gray-200">
-                    <i className="mr-1 fas fa-bookmark fa-xs"></i>
-                    <span className="font-semibold">Save</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <PostCard post={post} key={post.identifier} />
           ))}
         </div>
       </div>
@@ -115,19 +40,19 @@ export default function IndexPage({ posts }) {
     </div>
   );
 }
+// SSR Example
+// export const getServerSideProps: GetServerSideProps = async (
+//   context,
+// ) => {
+//   try {
+//     const res = await Axios.get('/posts');
 
-export const getServerSideProps: GetServerSideProps = async (
-  context,
-) => {
-  try {
-    const res = await Axios.get('/posts');
-
-    return { props: { posts: res.data } };
-  } catch (err) {
-    {
-      props: {
-        error: 'Something went wrong';
-      }
-    }
-  }
-};
+//     return { props: { posts: res.data } };
+//   } catch (err) {
+//     {
+//       props: {
+//         error: 'Something went wrong';
+//       }
+//     }
+//   }
+// };
