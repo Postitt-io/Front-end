725fe8a8be701d4220b154ad992c1ac12d22223c
Kristian Dunne <33926644+KristianDunne@users.noreply.github.com>
2020-12-23 12:20:06 +0000

    Revert "Refactor links used throughout site & use TypeScript (#11)"
    This reverts commit d1e4c461d95331007858dfd444c25cf853f88d9b.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,38 +6,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: 'media', // 'media' or 'class'
+  darkMode: 'class', // 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
         sans: ['Karla', ...defaultTheme.fontFamily.sans],
-        body: ['Karla'],
       },
       colors: {
         'accent-1': '#333',
         'button-blue': '#604E91',
       },
-      spacing: {
-        160: '40rem',
-      },
-      container: false,
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    ({ addComponents }) => {
-      addComponents({
-        '.container': {
-          width: '100',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          '@screen sm': { maxWidth: '640px' },
-          '@screen md': { maxWidth: '768px' },
-          '@screen lg': { maxWidth: '975px' },
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
